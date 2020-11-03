8a3a1baa336465470777fd5c0dd0dd0c68a03e99
Abid <abid-elfargougui@outlook.fr>
2020-11-03 17:56:58 +0100

    setup analytics gtag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import {
 	Switch,
 	Route,
 	Redirect
 } from 'react-router-dom';
 
-
 import HomePage from './pages/HomePage';
 
 const App = () => {
+
+	function initializeReactGA() {
+		ReactGA.initialize('G-X0D0H5VV93');
+		ReactGA.pageview('/home');
+	}
+	
 	return (
 		<>
 			<Switch>
